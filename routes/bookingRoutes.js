acd0e38838803472f0a3bffa8abb7109a4010d03
Aman Hussain <amanhussain134@gmail.com>
2023-04-02 22:58:30 +0530

    first commit

---
+++ routes/bookingRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const {
+	createBooking,
+	getBookings,
+	temp,
+	updateBooking,
+	deleteBooking,
+} = require("../controllers/bookingControllers");
+router.post("/booking", createBooking);
+router.get("/getbookings", getBookings);
+router.post("/temp", temp);
+router.post("/updatebooking", updateBooking);
+router.post("/deletebooking", deleteBooking);
+
+module.exports = router;
